152c39d15da21deafd6959ad9645e9d54118a45c
PretorianCC <pi_2002@mail.ru>
2022-05-18 12:27:59 +0600

    Add typeof

--- typescript/manipulating_types/typeof.ts
+++ typescript/manipulating_types/typeof.ts
@@ -1,3 +1,34 @@
 namespace TypeOf {
-  
+
+  let strOrNum: string | number;
+  if (Math.random() > 0.5) {
+    strOrNum = 5;
+  } else {
+    strOrNum = 'str';
+  }
+
+  // сужение типов
+  if (strOrNum === 'string') {
+    console.log(strOrNum); // string
+  } else {
+    console.log(strOrNum); // number
+  }
+
+  // копирование типа
+  let str20rNum: typeof strOrNum;
+  const user = {
+    name: 'Имя',
+    age: 23
+  }
+
+  // типы из ключей объектов
+  type keyOfUser = keyof typeof user;
+
+  enum Direction {
+    Up,
+    Down
+  }
+
+  // типы перечислений
+  type direct = keyof typeof Direction;
 }

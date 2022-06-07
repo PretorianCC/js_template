05cdeae65c76f1b72747fe3a51da7772aca93907
PretorianCC <pi_2002@mail.ru>
2022-06-07 20:49:30 +0600

    fix express middleware

--- lib/express/middleware/logger.ts
+++ lib/express/middleware/logger.ts
@@ -1,6 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 
-export function logger(res: Request, req: Response, next: NextFunction) {
+export function logger(res: Request, req: Response, next: NextFunction): void {
   console.log(`method: ${res.method}, path: ${res.path}`);
   next();
 }
--- lib/express/middleware/users.ts
+++ lib/express/middleware/users.ts
@@ -1,4 +1,9 @@
-export const users =[
+interface User {
+  id: number;
+  name: String;
+}
+
+export const users: User[] = [
   {id: 1, name: 'user 1'},
   {id: 2, name: 'user 2'},
   {id: 3, name: 'user 3'}

2dd61febf3886b21548bd121500f5d7d3823db52
PretorianCC <pi_2002@mail.ru>
2022-11-19 21:39:15 +0600

    primary types

---
+++ typescript/types/primaryTypes.ts
@@ -0,0 +1,25 @@
+namespace PrimaryTypes {
+
+  // Булево
+  const isAdmin = true;
+  const isUser = false;
+
+  // Число
+  const int: number = 42;
+  const float: number = 4.5;
+  const num: number = 3e10;
+
+  // Строка
+  const str: string = 'template';
+
+  // Массив
+  const arr: number[] = [1, 2, 3];
+  const arr2: Array<number> = [1, 2, 3];
+  
+  // Кортеж
+  const typle: [string, number] = ['test', 123];
+
+  // Любой тип
+  const variable: any = 23;
+
+}
